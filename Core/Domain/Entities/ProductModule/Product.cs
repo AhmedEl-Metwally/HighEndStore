4ec77863ecfeb8fe9abd9db51fed011d1b63703c
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-10-11 17:38:14 +0300

    Product & ProductBrand& ProductType Entities

---
+++ Core/Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Domain.Entities.ProductModule
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string PictureUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+
+        public int TypeId { get; set; }
+        public ProductType ProductType  { get; set; } = default!;
+
+        public int BrandId { get; set; }
+        public ProductBrand ProductBrand  { get; set; } = default!;
+    }
+}
